80879bf0137feedd20b3d6519fb0d7768a8fccb8
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-07-13 08:17:55 +0500

    final changes before screenshot

--- src/App.js
+++ src/App.js
@@ -16,11 +16,48 @@ function App() {
 	const [message, setMessage] = useState("");
 	const [isVisible, setVisible] = useState(false);
 
+	let pricePerPage = 260;
+	let pricePerPageColor = 370;
+	let coverPrice = 0;
+
+	const handlePrice = (text) => {
+		let bookNum;
+		if(text > 440 && text < 781){
+			bookNum = 2;
+		} else if (text > 780){
+			bookNum = Math.ceil((text - 780) / 390) + 2;
+		} else bookNum = 1;
+		text = Math.ceil(text / bookNum);
+		coverPrice = text >= 53 && text <= 129
+			? 13000
+			: text >= 130 && text <= 200
+				? 14000
+				: text >= 201 && text <= 300
+					? 15000
+					: text >= 301 && text <= 400
+						? 16000
+						: text >= 401 && text <= 440
+							? 17000 : 0;
+		let calcPriceA5 = (Math.ceil(text / 4) * pricePerPage + coverPrice) * bookNum;
+		calcPriceA5 = calcPriceA5 % 500 ? Math.ceil(calcPriceA5 / 500) * 500 : calcPriceA5;
+		setPriceA5(calcPriceA5);
+		let calcPriceA5Color = (Math.ceil(text / 4) * pricePerPageColor + coverPrice) * bookNum;
+		calcPriceA5Color = calcPriceA5Color % 500 ? Math.ceil(calcPriceA5Color / 500) * 500 : calcPriceA5Color;
+		setPriceA5Color(calcPriceA5Color);
+		let calcPriceA4 = (Math.ceil(text / 2) * pricePerPage + coverPrice) * bookNum;
+		calcPriceA4 = calcPriceA4 % 500 ? Math.ceil(calcPriceA4 / 500) * 500 : calcPriceA4;
+		setPriceA4(calcPriceA4);
+		let calcPriceA4Color = (Math.ceil(text / 2) * pricePerPageColor + coverPrice) * bookNum;
+		calcPriceA4Color = calcPriceA4Color % 500 ? Math.ceil(calcPriceA4Color / 500) * 500 : calcPriceA4Color;
+		setPriceA4Color(calcPriceA4Color);
+		setMessage(`Pereplyotlar soni: ${bookNum} `);
+	};
+
 	const handleText = (e) => {
 		if (text === "") {
 			setMessage(null);
 			setButton(true);
-		} else if (text !== "" && isNaN(text.trim())) {
+		} else if (text !== "" && isNaN(text)) {
 			setButton(true);
 			setMessage("Sahifalar soni faqat raqamlardan va kamida 10 bet bo'lishi kerak !!!");
 			setText("440");
@@ -33,55 +70,41 @@ function App() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		let pricePerPage = 260;
-		let pricePerPageColor = 370;
-		let coverPrice =
-			text >= 53 && text <= 129
-				? 12000
-				: text >= 130 && text <= 200
-				? 13000
-				: text >= 201 && text <= 300
-				? 14000
-				: text >= 301 && text <= 400
-				? 15000
-				: Math.ceil(text / 440) * 16000;
 
 		if (text >= 0 && text <= 52) {
-			setPriceA5(14500);
-			setPriceA4(17500);
-			setPriceA5Color(16000);
-			setPriceA4Color(20000);
+			setPriceA5(15500);
+			setPriceA4(18500);
+			setPriceA5Color(17000);
+			setPriceA4Color(21000);
 			setVisible(true);
 		}
-		if (text >= 53 && text <= 440) {
-			let calcPriceA5 = Math.ceil(text / 4) * pricePerPage + coverPrice;
-			calcPriceA5 = calcPriceA5 % 500 ? Math.ceil(calcPriceA5 / 500) * 500 : calcPriceA5;
-			setPriceA5(calcPriceA5);
-			let calcPriceA5Color = Math.ceil(text / 4) * pricePerPageColor + coverPrice;
-			calcPriceA5Color = calcPriceA5Color % 500 ? Math.ceil(calcPriceA5Color / 500) * 500 : calcPriceA5Color;
-			setPriceA5Color(calcPriceA5Color);
-			let calcPriceA4 = Math.ceil(text / 2) * pricePerPage + coverPrice;
-			calcPriceA4 = calcPriceA4 % 500 ? Math.ceil(calcPriceA4 / 500) * 500 : calcPriceA4;
-			setPriceA4(calcPriceA4);
-			let calcPriceA4Color = Math.ceil(text / 2) * pricePerPageColor + coverPrice;
-			calcPriceA4Color = calcPriceA4Color % 500 ? Math.ceil(calcPriceA4Color / 500) * 500 : calcPriceA4Color;
-			setPriceA4Color(calcPriceA4Color);
+		if (text >= 53) {
+			handlePrice(text);
 			setVisible(true);
-		}
-		if (text > 440) {
-			setText("");
-			setMessage("Sahifalar soni 440 tadan kam bo'lishi kerak");
-			setVisible(false);
 		}
 
-		// setText("");
 	};
+
+	function handleFile() {
+		let input = document.getElementById("file");
+		let reader = new FileReader();
+		reader.readAsBinaryString(input.files[0]);
+		reader.onloadend = function(){
+			let count = reader.result.match(/\/Type[\s]*\/Page[^s]/g).length;
+			setText(count + "");
+		}
+		setButton(false);
+	}
 
 	return (
 		<div className="App">
 			<form className="calculation" onSubmit={handleSubmit}>
 				<h1>WePrint</h1>
 				<h2>Biz bilan kitob chiqarish yanada oson</h2>
+				<label className="custom-file-upload">
+					File yuklash
+				<input type="file" placeholder="Upload it" onChange={handleFile} id='file'/>
+				</label>
 				<input type="text" placeholder="Sahifa sonini kiriting" onChange={handleText} value={text} />
 				<button type="submit" disabled={button}>
 					Hisoblash
@@ -96,6 +119,13 @@ function App() {
 						<p>** A4 format -- bu to'liq list hajmi.</p>
 						<p>** A5 format -- bu to'liq list hajmining yarmi.</p>
 					</div>
+				</div>
+				<div className="map">
+					<iframe title="uniqua;slkdjf;alksdjf"
+						src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5993.851118826498!2d69.246204!3d41.310483!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE4JzM3LjciTiA2OcKwMTQnNDYuMyJF!5e0!3m2!1sen!2s!4v1684607694203!5m2!1sen!2s"
+						loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade">
+					</iframe>
 				</div>
 				<ul className="links">
 					<li>
